8647717f6939df6be2e7f9cb0a786966e7bc76ff
Minjun <84955607+devkr-potato@users.noreply.github.com>
2022-03-31 18:58:49 +0900

    Add files via upload

---
+++ C-Moudle/js/review.js
@@ -0,0 +1,20 @@
+const get = (element) => {
+    return document.querySelector(element)
+};
+
+get('.write').addEventListener('click',()=>{
+    document.querySelector('.modal').style.display = 'block';
+});
+
+get('.modal-close').addEventListener('click',()=>{
+    document.querySelector('.modal').style.display = 'none';
+});
+
+window.addEventListener('keydown',(element)=>{
+    if(element.code == "Escape"){
+        document.querySelector('.modal').style.display = 'none';
+    }else if(element.code == "KeyP"){
+        document.querySelector('.modal').style.display = 'block';
+    }
+});
+
